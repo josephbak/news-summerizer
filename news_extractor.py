abecc9637b438f001850bfc77501663c8f4a3416
Joseph Bak <joseph.bak31415@gmail.com>
2023-01-26 18:24:17 -0500

    news_extractor module updated: techcrunch and reuters

--- news_extractor.py
+++ news_extractor.py
@@ -3,35 +3,11 @@ from bs4 import BeautifulSoup
 from newsplease import NewsPlease
 from typing import List
 
-
-#print(ap_topnews_list)
-
-# techcrunch
-techcrunch_url = 'https://techcrunch.com'
-r_tech = requests.get(techcrunch_url)
-
-soup_tech = BeautifulSoup(r_tech.content, 'html.parser')
-atag_tech = soup_tech.find_all('a', attrs={"class": "post-block__title__link"})
-
-tech_news_list = [i['href'] for i in atag_tech]
-#print(tech_news_list)
-
-# reuters news
-reuters_news_url = 'https://www.reuters.com'
-reuters_business_url = 'https://www.reuters.com/business/'
-r_reuters = requests.get(reuters_business_url)
-
-soup_reuters = BeautifulSoup(r_reuters.content, 'html.parser')
-atag_reuters = soup_reuters.find_all('a', attrs={"data-testid": "Heading"})
-
-reuters_business_list = [reuters_news_url + i['href'] for i in atag_reuters]
-#print(reuters_business_list)
-
 class ApNews:
     def __init__(self) -> None:
         pass
 
-    def get_news(self, number_of_article=1) -> List[str]:
+    def get_news_urls(self, number_of_article=1) -> List[str]:
         # ap news
         # request
         ap_news_url = 'https://apnews.com'
@@ -43,11 +19,53 @@ class ApNews:
         ap_topnews_list = [ap_news_url + i['href'] for i in atag_ap]
         return ap_topnews_list[:number_of_article]
     
-    def get_top_news(self) -> str:
-        return self.get_news()[0]
+    def get_top_news_url(self) -> str:
+        return self.get_news_urls()[0]
+
+class TechCrunch:
+    def __init__(self) -> None:
+        pass
+        
+    def get_news_urls(self, number_of_article=1) -> List[str]:
+        # techcrunch
+        techcrunch_url = 'https://techcrunch.com'
+        r_tech = requests.get(techcrunch_url)
+
+        soup_tech = BeautifulSoup(r_tech.content, 'html.parser')
+        atag_tech = soup_tech.find_all('a', attrs={"class": "post-block__title__link"})
+
+        tech_news_list = [i['href'] for i in atag_tech]
+        return tech_news_list[:number_of_article]
+        
+    def get_top_news_url(self) -> str:
+        return self.get_news_urls()[0]
+    
+class Reuters:
+    def __init__(self) -> None:
+        pass
+
+    def get_news_urls(self, number_of_article=1) -> List[str]:
+        # reuters news
+        reuters_news_url = 'https://www.reuters.com'
+        reuters_business_url = 'https://www.reuters.com/business/'
+        r_reuters = requests.get(reuters_business_url)
+
+        soup_reuters = BeautifulSoup(r_reuters.content, 'html.parser')
+        atag_reuters = soup_reuters.find_all('a', attrs={"data-testid": "Heading"})
+
+        reuters_business_list = [reuters_news_url + i['href'] for i in atag_reuters]
+
+        return reuters_business_list[:number_of_article]
+        
+    def get_top_news_url(self) -> str:
+        return self.get_news_urls()[0]
 
 
 ap = ApNews()
-print(type(ap.get_news()))
-print(ap.get_news())
-print(ap.get_top_news())
+print(ap.get_top_news_url())
+
+tech = TechCrunch()
+print(tech.get_top_news_url())
+
+reuters = Reuters()
+print(reuters.get_top_news_url())
